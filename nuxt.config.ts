ca7a45b92a2f29ee8c72e57051a55c385f3a9a0c
gimy <b.afatah@yahoo.com>
2024-01-22 13:54:41 +0800

    feat: added global config and env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,17 @@ export default defineNuxtConfig({
     ],
   },
   modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    functionBaseUrl:
+      process.env.NODE_ENV === "production"
+        ? `${process.env.BASE_URL}/.netlify/functions`
+        : "http://localhost:8888/.netlify/functions",
+    STRIPE_PK_DEV: process.env.STRIPE_PK_DEV,
+    STRIPE_PK_PROD: process.env.STRIPE_PK_PROD,
+    public: {
+      lang: "en-US",
+      STRAPI_API: process.env.STRAPI_API,
+      STRAPI_API_KEY: process.env.STRAPI_API_KEY,
+    },
+  },
 });
